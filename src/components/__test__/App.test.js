3c736b24826b94e9462da728692e78e0d2263149
akhtarvahid <akhtarvahid543@gmail.com>
2019-04-21 02:46:14 +0530

    enzyme started

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', ()=>{
-    const div = document.createAttribute('div');
+    const div = document.createElement('div');
 
     ReactDOM.render(<App />, div);
+    console.log(div.innerHTML)
 
     ReactDOM.unmountComponentAtNode(div);
 })
