47ffadefc7c1e0de39df0304d59ee92a23299784
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-03-29 23:47:19 +0530

    topic card component

--- src/components/TopicList/TopicList.tsx
+++ src/components/TopicList/TopicList.tsx
@@ -1,8 +1,6 @@
 import { useMemo, useState } from "react";
 import {
-  Badge,
   Button,
-  Card,
   Col,
   Form,
   Modal,
@@ -11,11 +9,10 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
-import styles from "./TopicList.module.css";
 import { Tag } from "../../types";
-import { colorGenerator } from "../common/helper";
+import TopicCard from "../TopicCard/TopicCard";
 
-type SimplifiedTopic = {
+export type SimplifiedTopic = {
   tags: Tag[];
   title: string;
   id: string;
@@ -136,40 +133,6 @@ export function TopicList({
   );
 }
 
-function TopicCard({ id, title, tags, createdAt }: SimplifiedTopic) {
-  const color = colorGenerator();
-  return (
-    <Card
-      as={Link}
-      to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
-      style={{ backgroundColor: `#${color}`}}
-    >
-      <Card.Body>
-        <Stack
-          gap={2}
-          className="align-items-center justify-content-center h-100"
-        >
-          <span className="fs-5">{title}</span>
-          {tags.length > 0 && (
-            <Stack
-              gap={1}
-              direction="horizontal"
-              className="justify-content-center flex-wrap"
-            >
-              {tags.map((tag) => (
-                <Badge className="text-truncate" key={tag.id}>
-                  {tag.label}
-                </Badge>
-              ))}
-            </Stack>
-          )}
-          <div>{createdAt}</div>
-        </Stack>
-      </Card.Body>
-    </Card>
-  );
-}
 
 function EditTagsModal({
   availableTags,
