63327b6bc38304db9675d795721628ee54e2ba12
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-04-04 00:38:25 +0530

    create topic note hook

--- src/hooks/useTopicNote.ts
+++ src/hooks/useTopicNote.ts
@@ -2,6 +2,31 @@ import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
 import { TOPIC_API } from "../utils/constant";
 
+// create new topicNote
+async function postRequest(url: RequestInfo | URL, { arg }: { arg: any }) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  }).then((res) => res.json());
+}
+
+export const usePostTopicNote = () => {
+  const { data, trigger, isMutating, error } = useSWRMutation(
+    `${TOPIC_API}/sticky`,
+    postRequest
+  );
+
+  return {
+    data,
+    trigger,
+    isMutating,
+    error,
+  };
+};
+
 // get all topicNotes
 export const getRequest = async (url: RequestInfo | URL) => {
   return fetch(url, {
